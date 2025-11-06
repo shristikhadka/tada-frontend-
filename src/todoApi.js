11033faa753efece6eff120f9bb3a11a50b41576
Shristi Khadka <shristikhadka0988@gmail.com>
2025-11-06 00:23:41 -0500

    create local storage to store  user info and send headers with api client

--- src/todoApi.js
+++ src/todoApi.js
@@ -1,8 +1,21 @@
 import axios from 'axios';
+import { getAuthToken } from './utils/auth';
 
-const API_BASE_URL="http://localhost:8080/api/todos";
+//create axios instance with auth interceptor
+const apiClient=axios.create({
+    baseURL:"http://localhost:8080",
+});
+ // Add auth header to every request
+ apiClient.interceptors.request.use((config)=>{
+    const token=getAuthToken();
+    if(token){
+        config.headers.Authorization=`Basic ${token}`;
+    }
+    return config;
+ });
 
-export const getAllTodos=()=>axios.get(API_BASE_URL);
-export const addTodo=(todo)=>axios.post(API_BASE_URL,todo);
-export const updateTodo=(id,updateTodo)=>axios.put(`${API_BASE_URL}/${id}`,updateTodo);
-export const deleteTodo=(id)=>axios.delete(`${API_BASE_URL}/${id}`);
+ // Export functions using the authenticated client
+export const getAllTodos=()=>apiClient.get('/user/todos');
+export const addTodo=(todo)=>apiClient.post('/user/todos',todo);
+export const updateTodo=(id,updateTodo)=>apiClient.put(`/user/todos/${id}`,updateTodo);
+export const deleteTodo=(id)=>apiClient.delete(`/user/todos/${id}`);
---
+++ src/utils/auth.js
@@ -0,0 +1,30 @@
+
+
+export const setAuthCredentials=(username,password)=>{
+    const credentials=btoa(`${username}:${password}`);
+    localStorage.setItem('authToken',credentials);
+    localStorage.setItem('username',username);
+};
+
+
+//remove credentials on  logout
+export const clearAuthCredentials=()=>{
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('username');
+};
+
+//get stored credentials
+export const getAuthToken=()=>{
+    return localStorage.getItem('authToken');
+};
+
+//get username
+export const getUsername=()=>{
+    return localStorage.getItem('username');
+};
+
+// Check if user is logged in
+export const isAuthenticated = () => {
+    return !!localStorage.getItem('authToken');
+};
+
